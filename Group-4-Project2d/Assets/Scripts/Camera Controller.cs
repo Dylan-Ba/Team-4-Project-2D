1fcc006db7b5c1ea5fb786d90d48e89aba2be059
Lexi <alexandriavye44@gmail.com>
2024-06-20 19:54:21 -0400

    Camera Tracking

--- Group-4-Project2d/Assets/Scripts/Camera Controller.cs
+++ Group-4-Project2d/Assets/Scripts/Camera Controller.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public GameObject player;
+    public float offset;
+    public float offsetSmoothing;
+    private Vector3 playerPosition;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        playerPosition = new Vector3 (player.transform.position.x, player.transform.position.y + 1f, transform.position.z);
+
+        if (player.transform.localScale.x > 0)
+        {
+            playerPosition = new Vector3(playerPosition.x + offset, player.transform.position.y + 1f, transform.position.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, player.transform.position.y + 1f, transform.position.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
     }
 }
